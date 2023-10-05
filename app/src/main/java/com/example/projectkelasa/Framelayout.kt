c55be735fc7e2d5d9d2d8db07586d22f3567c121
TC1034 <mahfudzsiddiq0@gmail.com>
2023-10-05 22:44:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectkelasa/Framelayout.kt
@@ -0,0 +1,11 @@
+package com.example.projectkelasa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Framelayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_framelayout)
+    }
+}
